7b964ba4d4eea52361643dd16b7e3162865ad7f9
viktor_shvets <shvetsviktor89@gmail.com>
2024-10-03 11:03:53 +0100

    correct URL has been added

--- publishconf.py
+++ publishconf.py
@@ -8,7 +8,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://this_is_test.com"
+SITEURL = "https://shvetsviktor.github.io/pelican_test_3"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
